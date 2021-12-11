6328e10bd0e5bbbfc9e605bfdc9b3f138988d85f
ashfakul101 <ashfakulkarim2@gmail.comgit config --global user.email ashfakulkarim2@gmail.com>
2021-12-11 10:48:33 +0600

    React Route v6 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/home" element={<Home/>}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {  Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../Images/logo/logo.png'
+import './Header.css';
+import icons from '../../../Images/login-svgrepo-com.svg'
 const Header = () => {
     return (
-        <div className="container">
-           <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <div className="container header-bg">
+           <Navbar collapseOnSelect expand="lg"    variant="light">
                 <Container>
                 <Navbar.Brand href="#home"><img src={logo} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -13,11 +15,15 @@ const Header = () => {
                    
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#deets">Home</Nav.Link>
-                    <Nav.Link >
+                    <Nav.Link className="header-nav-menu me-3">Home</Nav.Link>
+                    <Nav.Link  className="header-nav-menu me-3">
                        courses
                     </Nav.Link>
+                    <Nav.Link className="header-nav-menu">Login <img className='icons' src={icons} alt="" />
+                            
+                        </Nav.Link>
                     </Nav>
+                    
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
